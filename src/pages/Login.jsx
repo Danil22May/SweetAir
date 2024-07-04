707c8171378bec85e81f962bd76c405b2de3bfd3
Danil22May <daniilmej88@gmail.com>
2024-07-04 10:41:17 +0200

    adding basic styles to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,13 @@
+
 const Login  =() => {
 
     return (
         <>
+        <section className="h-96 border flex flex-col mx-auto w-96 gap-9">
+            <label htmlFor="city" className="text-4xl font-medium">Busca tu ciudad</label>
+            <input type="text" name="city" className="border-2 border-gray-400 p-2 rounded-md shadow-inner" />
+            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+        </section>
         </>
     )
 
