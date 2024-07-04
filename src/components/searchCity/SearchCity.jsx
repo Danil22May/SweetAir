9a43ccdd22d600754c01e7ea17edad9a665e0f13
Cris <crispr.zde@gmail.com>
2024-07-04 09:01:57 +0200

    Startng component

---
+++ src/components/searchCity/SearchCity.jsx
@@ -0,0 +1,12 @@
+
+const SeachCity = () => {
+    return(
+        <>
+        <h3>Busca tu ciudad</h3>
+        <div>
+            <input type="text" />
+            
+        </div>
+        </>
+    )
+}
