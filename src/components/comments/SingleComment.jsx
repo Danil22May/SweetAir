502116548b9ea075989e18e47103c4ed48d401e2
Betty Lopez <elisabetlopezpons@gmail.com>
2024-07-04 17:24:53 +0200

    Admin comments

--- src/components/comments/SingleComment.jsx
+++ src/components/comments/SingleComment.jsx
@@ -4,11 +4,12 @@ import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 
 
+
 const SingleComment = ({comment, admin}) => {
 
     return (
-        <div className="rounded-[22px] bg-[color:var(--col-dark)] w-[85%] py-4 px-3 flex flex-row space-around ">
-            <FontAwesomeIcon icon={faMessage} className="text-[color:var(--col-white)] my-2 mr-1"/>
+        <div className="rounded-[22px] bg-[color:var(--col-dark)] w-[85%] py-4 px-3 flex flex-row justify-around gap-1">
+                <FontAwesomeIcon icon={faMessage} className="text-[color:var(--col-white)] ml-1 mr-[0.1em]"/>
             <p className="text-[color:var(--col-white)] text-center text-light my-2">{comment}</p>
             <div className={`${admin} flex flex-col gap-7 fa-sm space-between pl-1`}>
                 <FontAwesomeIcon icon={faPen} className="text-[color:var(--col-white)] self-start "/>
--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -2,24 +2,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SingleComment from "../components/comments/SingleComment";
 import Navbar from "../components/logo/Navbar";
 import { faAngleDoubleLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import FormComments from "../components/comments/FormComments";
+
 
 
 const Comments = () => {
-    const comment = ['Soy un comentario sobre la calidad del aire de esta ciudad', 'Soy un comentario sobre la calidad del aire de esta ciudad', 'Soy un comentario sobre la calidad del aire de esta ciudad', 'Soy un comentario sobre la calidad del aire de esta ciudad']
+    const comment = ['Soy un comentario sobre la calidad del aire de esta ciudad', 'Soy un comentario sobre la calidad del aire de esta ciudad', 'Soy un comentario sobre la calidad del aire de esta ciudad', 'Soy un comentario sobre la calidad del aire de esta ciudad','Soy un comentario sobre la calidad del aire de esta ciudad', 'Soy un comentario sobre la calidad del aire de esta ciudad', 'Soy un comentario sobre la calidad del aire de esta ciudad']
 
     return (
         <div className="bg-white-gradient h-screen ">
             <Navbar />
-            <div className="px-[0.5em] mx-[1em] flex flex-col">
-                <div className="flex flex-row justify-between mx-3 content-center pb-2">
-                    <FontAwesomeIcon icon={faAngleDoubleLeft} className="text-[color:var(--col-dark)] self-center"/>
+            <div className="flex flex-col w-[90%] justify-center m-auto gap-2">
+                <div className="flex flex-row justify-between content-center pb-2">
+                    <Link to="/airquality">
+                        <FontAwesomeIcon icon={faAngleDoubleLeft} className="text-[color:var(--col-dark)] self-center"/>
+                    </Link>
                     <h1 className="uppercase text-[1.5em] self-center">Comments</h1>
                 </div>
-                <div className="m-auto flex flex-col items-center justify-center content-center bg-[color:var(--col-white)] rounded-[22px] mt-[1em] py-5 gap-4 drop-shadow-xl">
+                <div className="flex flex-col items-center content-center bg-[color:var(--col-purer-white)] rounded-[22px] mt-[0.5em] py-5 gap-3 drop-shadow-xl overflow-auto h-[30em] no-scrollbar">
                     {comment.map((comment, index) => (
                         <SingleComment key={index} comment={comment} admin="hidden"/>
                     ))}
                 </div>
+                    <FormComments />
             </div>
         </div>
     )
