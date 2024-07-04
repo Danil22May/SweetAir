4b2f4294fdcff238f1b08b9fa4fe7ae1cc8274f1
Betty Lopez <elisabetlopezpons@gmail.com>
2024-07-05 00:03:40 +0200

    Data and comments visualization fixed

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -27,11 +27,11 @@ const Comments = () => {
         <div className="bg-white-gradient lg:h-[100vh]" >
             <Navbar />
             <div className="flex flex-col w-[90%] lg:w-[80%] lg:h-auto h-[50%] justify-around m-auto gap-2 py-4">
-                <div className="self-end pb-2">
+                <div className="flex flex-row self-end pb-2">
                     <h1 className="uppercase text-[1.5em] self-end">Comments</h1>
                 </div>
                 <div className="flex flex-col lg:flex-row gap-5">
-                    <div className="flex items-center content-center bg-[color:var(--col-purer-white)] rounded-[22px] py-5 gap-3 drop-shadow-xl overflow-auto h-[30em] lg:w-[90%]">                
+                    <div className="flex flex-col items-center content-center bg-[color:var(--col-purer-white)] rounded-[22px] py-5 gap-3 drop-shadow-xl overflow-auto h-[30em] lg:w-[60%]">                
                     {comments.map((comment, index) => (
                         <SingleComment key={index} id = {comment.id} comment={comment.comment} author={comment.author} admin={false}/>
                     ))}
