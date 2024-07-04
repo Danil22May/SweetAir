f13f6fb0f7fe46a160c4554621e7535db8b134c7
Betty Lopez <elisabetlopezpons@gmail.com>
2024-07-04 12:39:13 +0200

    Navigation air quality

--- src/pages/airQuality/AirQualityMain.jsx
+++ src/pages/airQuality/AirQualityMain.jsx
@@ -1,8 +1,10 @@
+import AirCityInfo from "./airCityInfo/AirCityInfo";
+
 const AirQualityMain = () => {
 
     return (
         <div className="w-screen h-screen bg-green-gradient">
-            
+            <AirCityInfo qualityNumber="47" city="Oviedo" status="buena"/>
         </div>
     )
 }
