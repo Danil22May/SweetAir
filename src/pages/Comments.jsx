9ee1aec10337064e073c8d20f0033254e08e0bde
Betty Lopez <elisabetlopezpons@gmail.com>
2024-07-04 19:35:29 +0200

    fix merge losses

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -32,13 +32,10 @@ const Comments = () => {
         <div className="bg-white-gradient h-screen ">
             <Navbar />
             <div className="flex flex-col w-[90%] justify-center m-auto gap-2">
-                <div className="flex flex-row justify-between content-center pb-2">
-                    <Link to="/airquality">
-                        <FontAwesomeIcon icon={faAngleDoubleLeft} className="text-[color:var(--col-dark)] self-center"/>
-                    </Link>
-                    <h1 className="uppercase text-[1.5em] self-center">Comments</h1>
+                <div className="flex flex-row content-center self-end pb-2">
+                    <h1 className="uppercase text-[1.5em] self-end">Comments</h1>
                 </div>
-                <div className="flex flex-col items-center content-center bg-[color:var(--col-purer-white)] rounded-[22px] mt-[0.5em] py-5 gap-3 drop-shadow-xl overflow-auto h-[30em] no-scrollbar">
+                <div className="flex flex-col justify-center content-center bg-[color:var(--col-purer-white)] rounded-[22px] mt-[0.5em] py-5 gap-3 drop-shadow-xl overflow-auto h-[30em] no-scrollbar">
                 {comments.map((comment, index) => (
                     <SingleComment key={index} comment={comment.comment} author={comment.author} admin="hidden"/>
                 ))}
