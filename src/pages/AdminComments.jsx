21752603c90b69290719333ce663b1e1a64aefc4
Betty Lopez <elisabetlopezpons@gmail.com>
2024-07-04 23:24:45 +0200

    Fixing responsive of admin comments page

--- src/pages/AdminComments.jsx
+++ src/pages/AdminComments.jsx
@@ -29,22 +29,23 @@ const AdminComments = () => {
     };
 
     return (
-        <div className="bg-white-gradient h-screen ">
-            <Navbar />
-            <div className="flex flex-col w-[85%] h-[90%] justify-center m-auto gap-2">
-                <div className="flex flex-row justify-between content-center pb-2">
-                    <Link to="/search">
-                        <FontAwesomeIcon icon={faAngleDoubleLeft} className="text-[color:var(--col-dark)] self-center"/>
-                    </Link>
-                    <h1 className="uppercase text-[1.5em] self-center">Comments</h1>
-                </div>
-                <div className="flex flex-col items-center content-center bg-[color:var(--col-purer-white)] rounded-[22px] mt-[0.5em] py-5 gap-3 drop-shadow-xl overflow-auto h-[30em]">
-                    {comments.map((comment, index) => (
+        <div className="bg-white-gradient lg:h-[100vh]" >
+        <Navbar />
+        <div className="flex flex-col w-[90%] lg:w-[80%] lg:h-auto h-[50%] justify-around m-auto gap-2 py-4">
+            <div className="self-end pb-2">
+                <h1 className="uppercase text-[1.5em] self-end">Comments</h1>
+            </div>
+            <div className="flex flex-col lg:flex-row gap-5">
+                <div className="flex items-center content-center bg-[color:var(--col-purer-white)] rounded-[22px] py-5 gap-3 drop-shadow-xl overflow-auto h-[30em] lg:w-[90%]">                
+                {comments.map((comment, index) => (
                         <SingleComment key={index} id = {comment.id} comment={comment.comment} author={comment.author} admin={true}/>
                     ))}
                 </div>
+                <FormComments />
             </div>
         </div>
+    </div>
+
     )
 }
 
