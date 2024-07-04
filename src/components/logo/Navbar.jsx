039414334dbd8317c755c914a8e6f668f8efb51d
Betty Lopez <elisabetlopezpons@gmail.com>
2024-07-04 09:01:38 +0200

    Circle icons

--- src/components/logo/Navbar.jsx
+++ src/components/logo/Navbar.jsx
@@ -8,9 +8,12 @@ const Navbar = () => {
 
     return (
         <div className="flex flex-row w-[100%] justify-between content-center">
-            <FontAwesomeIcon icon={faSearch} className="text-[color:var(--col-dark)] m-[3em] self-start fa-lg" />
-            <FontAwesomeIcon icon={faUser} className="text-[color:var(--col-dark)] m-[2em] self-end fa-2x" />
-                
+            <div className="w-[40px] h-[40px] bg-[color:var(--col-dark)] rounded-[50%] m-[3em] flex content-center justify-center ">
+                <FontAwesomeIcon icon={faSearch} className="text-[color:var(--col-dark)]  self-start fa-lg" />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faUser} className="text-[color:var(--col-dark)] m-[2em] self-end fa-2x" />
+            </div>
         </div>
     )
 }
